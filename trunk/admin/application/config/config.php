434c8dad839d9ac5025530aede64e86657d5bdbf
selviway2care@gmail.com <selviway2care@gmail.com@f68013a4-a764-4b14-cfc3-e40626648592>
2014-03-16 15:01:08 +0000

    completed.

--- trunk/admin/application/config/config.php
+++ trunk/admin/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'localhost/admin/';
+$config['base_url']	= 'http://localhost/way2care/admin';
 
 /*
 |--------------------------------------------------------------------------
